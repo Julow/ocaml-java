9a2e083e9663e95dcabca0cc969a261d7375b409
juloo <juloo.dsi@gmail.com>
2018-01-08 16:11:45 +0100

    Tests

---
+++ tests/test.ml
@@ -0,0 +1,19 @@
+let test_obj = object
+	method test = print_endline "OCaml method called"
+	method test_int a b = a + b
+end
+
+let () =
+	Callback.register "test_function" (fun () -> print_endline "OCaml function called");
+	Callback.register "test_raise" (fun () -> failwith "failuuure");
+	Callback.register "test_int" (+);
+	Callback.register "test_float" (+.);
+	Callback.register "test_string" (^);
+	Callback.register "test_bool" (&&);
+	Callback.register "test_int32" Int32.add;
+	Callback.register "test_int64" Int64.add;
+	Callback.register "get_value" (fun () -> ("a", "b"));
+	Callback.register "test_a" fst;
+	Callback.register "test_b" snd;
+	Callback.register "get_obj" (fun () -> test_obj);
+	print_endline "OCaml loaded"
---
+++ tests/test/Test.java
@@ -0,0 +1,169 @@
+package test;
+
+import juloo.javacaml.Caml;
+import juloo.javacaml.Callback;
+import juloo.javacaml.Value;
+import juloo.javacaml.CallbackNotFoundException;
+import juloo.javacaml.CamlException;
+import juloo.javacaml.InvalidMethodIdException;
+
+public class Test
+{
+	static
+	{
+		System.loadLibrary("test");
+		Caml.startup();
+	}
+
+	private static void test()
+	{
+// getCallback
+		Caml.getCallback("test_function");
+		try { Caml.getCallback(null); assert false; }
+		catch (NullPointerException e) {}
+		try { Caml.getCallback("not found"); assert false; }
+		catch (CallbackNotFoundException e) {}
+// hashVariant
+		Caml.hashVariant("test");
+		try { Caml.hashVariant(null); assert false; }
+		catch (NullPointerException e) {}
+
+// function
+		Caml.function(Caml.getCallback("test_function"));
+		Caml.argUnit();
+		Caml.callUnit();
+
+		try { Caml.function((Value)null); assert false; }
+		catch (NullPointerException e) {}
+		try { Caml.function((Callback)null); assert false; }
+		catch (NullPointerException e) {}
+		try { Caml.argString(null); assert false; }
+		catch (NullPointerException e) {}
+		try { Caml.argValue(null); assert false; }
+		catch (NullPointerException e) {}
+
+// CamlException
+		try
+		{
+			Caml.function(Caml.getCallback("test_raise"));
+			Caml.argUnit();
+			Caml.callUnit();
+			assert false;
+		} catch (CamlException e) {}
+
+// arg*/call*
+		Caml.function(Caml.getCallback("test_int"));
+		Caml.argInt(12);
+		Caml.argInt(3);
+		assert Caml.callInt() == 15;
+
+		Caml.function(Caml.getCallback("test_float"));
+		Caml.argFloat(1.0);
+		Caml.argFloat(1.0);
+		assert Caml.callFloat() == 2.0;
+
+		Caml.function(Caml.getCallback("test_string"));
+		Caml.argString("ab");
+		Caml.argString("cd");
+		assert Caml.callString().equals("abcd");
+
+		Caml.function(Caml.getCallback("test_bool"));
+		Caml.argBool(true);
+		Caml.argBool(true);
+		assert Caml.callBool();
+
+		Caml.function(Caml.getCallback("test_bool"));
+		Caml.argBool(true);
+		Caml.argBool(false);
+		assert !Caml.callBool();
+
+		Caml.function(Caml.getCallback("test_bool"));
+		Caml.argBool(true);
+		Caml.argBool(false);
+		assert !Caml.callBool();
+
+		Caml.function(Caml.getCallback("test_int32"));
+		Caml.argInt32(40);
+		Caml.argInt32(55);
+		assert Caml.callInt32() == 95;
+
+		Caml.function(Caml.getCallback("test_int64"));
+		Caml.argInt64(14);
+		Caml.argInt64(3);
+		assert Caml.callInt64() == 17;
+
+// argValue/callValue
+		Caml.function(Caml.getCallback("get_value"));
+		Caml.argUnit();
+		Value v = Caml.callValue();
+
+		Caml.function(Caml.getCallback("test_a"));
+		Caml.argValue(v);
+		assert Caml.callString().equals("a");
+
+		Caml.function(Caml.getCallback("test_b"));
+		Caml.argValue(v);
+		assert Caml.callString().equals("b");
+
+		Caml.function(Caml.getCallback("test_a"));
+		Caml.argValue(v);
+		Value a = Caml.callValue();
+		Caml.function(Caml.getCallback("test_b"));
+		Caml.argValue(v);
+		Value b = Caml.callValue();
+		Caml.function(Caml.getCallback("test_string"));
+		Caml.argValue(a);
+		Caml.argValue(b);
+		assert Caml.callString().equals("ab");
+
+// method
+		Caml.function(Caml.getCallback("get_obj"));
+		Caml.argUnit();
+		Value obj = Caml.callValue();
+
+		Caml.method(obj, Caml.hashVariant("test"));
+		Caml.callUnit();
+
+		Caml.method(obj, Caml.hashVariant("test_int"));
+		Caml.argInt(1);
+		Caml.argInt(2);
+		assert Caml.callInt() == 3;
+
+		try { Caml.method(obj, Caml.hashVariant("invalid")); assert false; }
+		catch (InvalidMethodIdException e) {}
+		try { Caml.method(null, 0); assert false; }
+		catch (NullPointerException e) {}
+
+// currying
+		Caml.function(Caml.getCallback("test_int"));
+		Caml.argInt(1);
+		Value plusOne = Caml.callValue();
+
+		Caml.function(plusOne);
+		Caml.argInt(1);
+		assert Caml.callInt() == 2;
+
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			assert false;
+			System.out.println("Cannot test, asserts are disabled");
+			System.exit(1);
+		}
+		catch (AssertionError e) {}
+		try
+		{
+			test();
+		}
+		catch (Exception e)
+		{
+			System.out.println("ERROR");
+			e.printStackTrace();
+			assert false;
+		}
+		System.out.println("Ok");
+	}
+}
