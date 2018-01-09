7430bd1b9b24a70241033f0d1a4aa4e89fb385f4
juloo <juloo.dsi@gmail.com>
2018-01-09 15:02:29 +0100

    Java.obj type, Java.Class.find_class

--- camljava/Java.ml
+++ camljava/Java.ml
@@ -1,6 +1,13 @@
 (** Interfaces Java
 	Unsafe *)
 
+(** Represent a java object
+	Does not support hash and marshalling *)
+type obj
+
+(** The null value *)
+let null : obj = (Obj.magic 0)
+
 (** Spawn a JVM and initialise the library
 	Takes an array of JVM options *)
 external startup : string array -> unit = "ocaml_java__startup"
@@ -9,3 +16,14 @@ external shutdown : unit -> unit = "ocaml_java__shutdown"
 let init opts =
 	startup opts;
 	at_exit shutdown
+
+module Class =
+struct
+
+	type t
+
+	(** `find_class s` returns the class named `s`
+		Raises `Not_found` if the class does not exists *)
+	external find_class : string -> t = "ocaml_java__find_class"
+
+end
--- camljava/java.c
+++ camljava/java.c
@@ -4,10 +4,86 @@
 #include <caml/callback.h>
 #include <caml/memory.h>
 #include <caml/fail.h>
+#include <caml/custom.h>
 
 #define JNI_VERSION JNI_VERSION_1_4
 
 static JNIEnv *env;
+
+/*
+** ========================================================================== **
+** OCaml value that represent a `jobject` pointer or the `null` value
+** -
+** The `jobject` is registered as global ref
+** and automatically released when it is garbage collected
+** -
+** Java_obj_val(v)		Returns the `jobject` pointer, must not be `null`
+** Java_null_val		`null` value
+** alloc_java_obj(obj)	Allocates the value, `obj` is registered as global ref
+*/
+
+#define Java_obj_val(v)	(*(jobject*)Data_custom_val(v))
+
+#define Java_null_val	(Val_int(0))
+
+static void java_obj_finalize(value v)
+{
+	if (v != Java_null_val)
+		(*env)->DeleteGlobalRef(env, Java_obj_val(v));
+}
+
+static int java_obj_compare(value a, value b)
+{
+	intnat a_;
+	intnat b_;
+
+	a_ = (a == Java_null_val) ? 0 : (intnat)Java_obj_val(a);
+	b_ = (b == Java_null_val) ? 0 : (intnat)Java_obj_val(b);
+	return a_ - b_;
+}
+
+static struct custom_operations java_obj_custom_ops = {
+	.identifier = "ocaml_java__obj",
+	.finalize = java_obj_finalize,
+	.compare = java_obj_compare,
+	.compare_ext = custom_compare_ext_default,
+	.hash = custom_hash_default,
+	.serialize = custom_serialize_default,
+	.deserialize = custom_deserialize_default
+};
+
+static value alloc_java_obj(jobject object)
+{
+	value v;
+
+	if (object == NULL)
+		return Java_null_val;
+	v = alloc_custom(&java_obj_custom_ops, sizeof(jobject), 0, 1);
+	object = (*env)->NewGlobalRef(env, object);
+	*(jobject*)Data_custom_val(v) = object;
+	return v;
+}
+
+/*
+** ========================================================================== **
+** Class
+*/
+
+value ocaml_java__find_class(value name)
+{
+	jclass c;
+
+	c = (*env)->FindClass(env, String_val(name));
+	if (c == NULL)
+		caml_raise_not_found();
+	return alloc_java_obj(c);
+}
+
+/*
+** ========================================================================== **
+** Init
+*/
+
 static JavaVM *jvm;
 
 value ocaml_java__startup(value opt_array)
